9103e841d5cda1a5d2d7fd485c5aa7b181e19893
Ankit Srivastava <42239246+ankitsri98@users.noreply.github.com>
2020-02-09 11:04:46 +0530

    Update Preorder_Traversal.cpp

--- Binary tree/Preorder_Traversal.cpp
+++ Binary tree/Preorder_Traversal.cpp
@@ -1,1 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+class node{
+    public:
+    int data;
+    node* left;
+    node* right;
+    
+    node(int d)
+    {
+        data=d;left=NULL,right=NULL;
+    }
+};
+
+node* buildtree(){
+    int d;
+    cin>>d;
+    
+    if(d==-1){ 
+        //-1 means end
+        return NULL;
+    }
+    
+    node * root=new node(d);
+    //root->left= as i am storing values
+    //values will be stored at the time of returning 
+    root->left=buildtree();//left subtree
+    root->right=buildtree();//right subtree
+    cout<<d<<" ";
+    return root;
+}
+
+void printtree(node* root)
+{
+    if(root=NULL){
+        return;
+    }
+    cout<<root->data<<" ";
+    printtree(root->left);
+    printtree(root->right);
+}
+int main() {
+	node* root=buildtree();
+	printtree(root);
+    
+	return 0;
+}
