ae8381486811fd392076606ef9ad58a24d9fdb13
Ankit Srivastava <42239246+ankitsri98@users.noreply.github.com>
2020-10-14 08:52:26 +0530

    light oj problem

---
+++ Digit Dp/Digit Sum.cpp
@@ -0,0 +1,43 @@
+/*
+
+
+Given a set of digits S, and an integer n, you have to find how many n-digit integers are there, 
+which contain digits that belong to S and the difference between any two adjacent digits is not more than two.
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int 
+
+ll dp[12][12];//pos prev
+ll g(int n,vector<ll > &v ,ll pos=0,ll prev=0,string s=""){
+    if(dp[pos][prev]!=-1){
+        return dp[pos][prev];
+    }
+    if(pos==n){
+        //cout<<s<<endl;
+        return 1;
+    }
+    ll tot=0;
+    for(int i=0;i<v.size();i++){
+        if(prev==0 || abs(prev-v[i])<=2){
+            tot+=g(n,v,pos+1,v[i],s+to_string(v[i]));
+        }
+    }
+    return  dp[pos][prev]=tot;
+}
+int main() {
+	ll n,l,r;
+	cin>>n;
+	for(int j=1;j<=n;j++){
+	    cin>>l>>r;
+	    vector<ll  > v(l);
+	    for(int i=0;i<l;i++){
+	        cin>>v[i];
+	    }
+	    memset(dp,-1,sizeof(dp));
+	    ll p=g(r,v);
+	    cout<<"Case "<<j<<":"<<" "<<p<<endl;
+    }
+	return 0;
+}
